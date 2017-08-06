e436548a658cd437f44a64dba1319cad028e0133
Kye Bracey <kye.bracey@gmail.com>
2017-08-06 19:42:18 +0100

    Adds Game#print_board method (private), updates Game#start to use this method instead of printing a hard-coded empty board

--- lib/game.rb
+++ lib/game.rb
@@ -6,8 +6,14 @@ class Game
   end
 
   def start
-    puts '| | | |'
-    puts '| | | |'
-    puts '| | | |'
+    print_board
+  end
+
+  private
+  def print_board
+    board = @board.map { |value| " " if value.nil? }
+    puts "|#{board[0]}|#{board[1]}|#{board[2]}|"
+    puts "|#{board[3]}|#{board[4]}|#{board[5]}|"
+    puts "|#{board[6]}|#{board[7]}|#{board[8]}|"
   end
 end
