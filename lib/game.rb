f57cb54bd24f52da479d6f0605f515539fa88c41
Kye Bracey <kye.bracey@gmail.com>
2017-08-06 19:54:07 +0100

    Updates Game#play to update the board with the current player's symbol (ie it now switches turns)

--- lib/game.rb
+++ lib/game.rb
@@ -3,10 +3,17 @@ class Game
 
   def initialize
     @board = Array.new(9)
+    @players = [:X, :O]
+    @current_turn = @players[0]
   end
 
   def start
     print_board
+  end
+
+  def play(field)
+    @board[field] = @current_turn
+    @current_turn = @players.select { |player| player != @current_turn }.first
   end
 
   private
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -13,4 +13,17 @@ describe Game do
       game.start
     end
   end
+
+  describe '#play' do
+    it 'updates the specified field with "X" on the first turn' do
+      game.play(0)
+      expect(game.board).to eq([:X, nil, nil, nil, nil, nil, nil, nil, nil])
+    end
+
+    it 'updates the specified field with "X" on the first turn and "0" on the second turn' do
+      game.play(5)
+      game.play(8)
+      expect(game.board).to eq([nil, nil, nil, nil, nil, :X, nil, nil, :O])
+    end
+  end
 end
